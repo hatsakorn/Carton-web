30e6f5222c9e1c68bd536a520c3633dcda182891
Unspotseed <nasuwan.meesuwan@gmail.com>
2023-02-18 16:34:19 +0700

    set up home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import { useState } from "react";
-import Modal from "./components/Modal";
-import LoginPage from "./page/LoginPage";
-
 function App() {
-  const [open, setOpen] = useState(false);
-
   return (
     <>
-      <div>
-        <button onClick={() => setOpen(true)} className="bg-blue-500">
-          Modal
-        </button>
-        <Modal open={open} onClose={() => setOpen(false)} title={"Modal"}>
-          <LoginPage />
-        </Modal>
-      </div>
+      <div></div>
     </>
   );
 }
