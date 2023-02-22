4f6b733f7bb229376472289375d958e066fa648e
hatsakorn <gornnb@gmail.com>
2023-02-22 12:31:04 +0700

    fix:Navsidebar

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -6,29 +6,6 @@ export default function HomePage() {
 
   return (
     <div className="flex justify-between">
-      <div className="flex-col flex justify-evenly text-center w-20">
-        <div className=" hover:bg-blue-400 m-1 rounded-xl shadow-xl ">
-          <i className="fa-solid fa-circle-user text-zinc-50 text-3xl m-4"></i>
-        </div>
-
-        <div className=" hover:bg-blue-400 m-1 rounded-xl shadow-xl">
-          <i clasName="fa-solid fa-house  text-zinc-50 text-3xl m-4"></i>
-        </div>
-
-        <div className=" hover:bg-blue-400 m-1 rounded-xl shadow-xl">
-          <i className="fa-solid fa-dolly  text-zinc-50 text-3xl m-4"></i>
-        </div>
-
-        <div className=" hover:bg-blue-400 m-1 rounded-xl shadow-xl">
-          <i className="fa-solid fa-car  text-zinc-50 text-3xl m-4"></i>
-        </div>
-        <div className=" hover:bg-blue-400 m-1 rounded-xl shadow-xl">
-          <i className="fa-solid fa-qrcode  text-zinc-50 text-3xl m-4"></i>
-        </div>
-        <div className=" hover:bg-blue-400 m-1 rounded-xl shadow-xl">
-          <i className="fa-solid fa-gear  text-zinc-50 text-3xl m-4"></i>
-        </div>
-      </div>
       <div className="flex justify-between bg-gradient-to-r bg-white  rounded-l-xl shadow-md w-full">
         <div className="flex-row">
           <div className="flex">sdf</div>
