c724465d0ef2e7f582da0fbb89ec1e49a28ed861
aisukdl <pk_aisu@hotmail.co.th>
2023-02-23 13:05:56 +0700

    remove and add all files

--- src/api/auth-api.js
+++
@@ -1,8 +0,0 @@
-import axios from "../config/axios";
-
-// export const login = (input) => axios.post("/auth/login", input);
-export const register = (input) => axios.post("auth/customerRegister", input);
-export const registerEmployee = (input) =>
-  axios.post("/auth/employeeRegister", input);
-export const login = (input) => axios.post("auth/login", input);
-export const getMe = () => axios.get("/auth/me");
