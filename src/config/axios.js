5fab306972f066a1ceda292183dd279126ceed5d
Unspotseed <nasuwan.meesuwan@gmail.com>
2023-02-23 14:39:20 +0700

    fix dot env

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { getAccessToken } from "../utils/local-storage";
 
 axios.defaults.baseURL = process.env.REACT_APP_ENDPOINT_URL;
+// axios.defaults.baseURL = " http://localhost:8304";
 
 axios.interceptors.request.use(
   (config) => {
