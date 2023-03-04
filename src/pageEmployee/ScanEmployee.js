1c3b8b648b28f4dc180da246bf0c4474c4c7a8df
aisukdl <pk_aisu@hotmail.co.th>
2023-03-04 13:45:13 +0700

    feat : install and test qrcode reader via camera

--- src/pageEmployee/ScanEmployee.js
+++ src/pageEmployee/ScanEmployee.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import Modal from "../components/Modal";
 import QrEmployee from "../QR/qrEmployee";
+import { useZxing } from "react-zxing";
 
 export default function ScanEmployee() {
   const [open, setOpen] = useState(false);
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onResult(result) {
+      setResult(result.getText());
+    },
+  });
 
   return (
     <div className="flex justify-center bg-gradient-to-r bg-white  rounded-l-xl shadow-md w-full">
       <div className="relative flex flex-col justify-center min-h-screen overflow-hidden h-14 mr-20">
         <div>
+        <video ref={ref} />
+        <p>
+          <span>Last result:</span>
+          <span>{result}</span>
+        </p>
           <button
             onClick={() => setOpen(true)}
             className="bg-sky-600 rounded m-3 p-1 hover:opacity-70 text-white "
