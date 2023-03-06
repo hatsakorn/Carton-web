7d42644b29015bfe6379ec3faa2109b780087407
Titipat_Mekmana <titipatmekmana@gmail.com>
2023-03-06 17:16:52 +0700

    fix uncommit router

--- src/components/NavSideBar.js
+++ src/components/NavSideBar.js
@@ -10,33 +10,6 @@ function NavSideBar() {
     fetchAuthUser();
   }, []);
   return (
-<<<<<<< HEAD
-    <div className="flex-col text-center w-20 justify-between">
-      <div className="flex m-1 rounded-xl bottom-25 left-0 ">
-        <i className="fa-solid fa-circle-user text-zinc-50 text-3xl m-4"></i>
-      </div>
-
-      <div className="flex hover:bg-blue-400 m-1 rounded-xl shadow-xl  ">
-        <Link to={"/homeAdmin"}>
-          <i className="fa-solid fa-house text-zinc-50 text-3xl m-4"></i>
-        </Link>
-      </div>
-
-      <div className="flex hover:bg-blue-400 m-1 rounded-xl shadow-xl  ">
-        <Link to={"/package"}>
-          <i className="fa-solid fa-dolly text-zinc-50 text-3xl m-4"></i>
-        </Link>
-      </div>
-
-      <div className="flex hover:bg-blue-400 m-1 rounded-xl shadow-xl ">
-        <Link to={"/assign"}>
-          <i className="fa-solid fa-paper-plane text-zinc-50 text-3xl m-4"></i>
-        </Link>
-      </div>
-      <div className="flex hover:bg-blue-400 m-1 rounded-xl shadow-xl ">
-        <Link to={"/scan"}>
-          <i className="fa-solid fa-qrcode text-zinc-50 text-3xl m-4"></i>
-=======
     <div className="flex-col flex justify-evenly text-center w-20">
       <div className="m-1 rounded-xl">
         <i className="fa-solid fa-circle-user text-zinc-50 text-3xl m-4" />
@@ -84,7 +57,6 @@ function NavSideBar() {
       <div className=" hover:bg-blue-400 m-1 rounded-xl shadow-xl">
         <Link to={authenticatedUser?.role === "EMPLOYEE" ? "/scan" : "/home"}>
           <i className="fa-solid fa-qrcode  text-zinc-50 text-3xl m-4"></i>
->>>>>>> develop
         </Link>
       </div>
       <div className="flex hover:bg-blue-400 m-1 rounded-xl shadow-xl ">
